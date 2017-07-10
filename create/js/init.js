e7f8246f944d904bac9bdca540131cfa3454cf7c
Fred Grott <fred.grott@gmail.com>
2017-07-10 14:18:57 -0500

    cahngelog v1.0.0
    changelog v1.0.0

---
+++ create/js/init.js
@@ -0,0 +1,40 @@
+(function($){
+  $(function(){
+  "use strict";
+
+  var window_width = $(window).width();
+
+
+
+
+
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
+    $('.carousel').carousel();
+    $('.slider').slider({full_width: true});
+    $('.parallax').parallax();
+    $('.modal').modal();
+    $('.scrollspy').scrollSpy();
+    $('.button-collapse').sideNav({'edge': 'left', menuWidth: '240'});
+    $('.datepicker').pickadate({selectYears: 20});
+    $('select').not('.disabled').material_select();
+    $('.myheader').lettering('words').children('span').lettering();
+    $('.sectionheaderone').lettering('words').children('span').lettering();
+
+    // Detect touch screen and enable scrollbar if necessary
+      function is_touch_device() {
+        try {
+          document.createEvent("TouchEvent");
+          return true;
+        }
+        catch (e) {
+          return false;
+        }
+      }
+      if (is_touch_device()) {
+        $('#nav-mobile').css({
+          overflow: 'auto'
+        })
+      }
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
